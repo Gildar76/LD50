597b3f58f4eac35a2feaef04dc72e111a409f9f6
gildar76 <gildar76@gmail.com>
2022-04-03 02:06:17 +0200

    Use colliders and raycasts for movement instead of the grid

--- Assets/_LD50/Scripts/PlayerController.cs
+++ Assets/_LD50/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ namespace GildarGaming.LD50
 {
     public class PlayerController : MonoBehaviour
     {
+        public LayerMask collissionMask;
         Grid2D grid;
         ParticleSystem waterEffect;
         public float speed = 0.25f;
@@ -25,12 +26,18 @@ namespace GildarGaming.LD50
             if (movement.y > 0)
             {
                 Vector3 newPosition = transform.position + transform.up * speed * Time.deltaTime; ;
-                if (grid.IsWalkable((int)newPosition.x, (int)newPosition.y))
+                if (!Physics2D.Raycast(transform.position, transform.up, speed * Time.deltaTime, collissionMask))
                 {
                     transform.position = newPosition;
                 }
+                Debug.DrawRay(transform.position, transform.up * newPosition.y, Color.red);
 
-                
+                //if (grid.IsWalkable((int)newPosition.x, (int)newPosition.y))
+                //{
+                //    
+                //}
+
+
             }
             
             if (Input.GetKeyDown(KeyCode.Space))
@@ -38,6 +45,12 @@ namespace GildarGaming.LD50
 
                     waterEffect.Play();
                 
+            }
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+
+                waterEffect.Stop();
+
             }
         }
 
